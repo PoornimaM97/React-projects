62955f43f9db02e6fd203aeeee84b84c1ebd9f16
Poornima.M <poornimamahadev01@gmail.com>
2024-11-20 17:16:43 +0530

    third commit

---
+++ reviews/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Reviews from './Reviews';
+
+
+function App() {
+  return (
+   <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Reviews/>
+      </section>
+      
+   </main>
+  );
+}
+
+export default App;
